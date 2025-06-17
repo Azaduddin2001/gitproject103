745433d870ad28d2b573e0862da683ece5e2734d
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-06-18 02:31:13 +0600

    gitproject103 Second comment

--- javagitproject103.java
+++ javagitproject103.java
@@ -4,5 +4,8 @@
         System.out.println("Hello, Git!");
         System.out.println("This is a Java project with Git integration.");
         System.out.println("Let's explore version control with Git.");
+        System.out.println("This is a simple Java application to demonstrate Git usage.");
+        System.out.println("Remember to commit your changes regularly.");
+        System.out.println("You can push your changes to a remote repository.");
     }
 }
